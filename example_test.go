36e4506b95d8b2efd19e0c0207fe758d81858302
Chris Stockton <chrisstocktonaz@gmail.com>
2017-04-01 14:55:06 -0700

    Add an example for decoding events during runtime.

--- example_test.go
+++ example_test.go
@@ -2,9 +2,15 @@ package trace_test
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"reflect"
+	"runtime"
+	"runtime/trace"
+	"sync"
+	"time"
 
-	trace "github.com/cstockton/go-trace"
+	"github.com/cstockton/go-trace/encoding"
 	"github.com/cstockton/go-trace/event"
 )
 
@@ -16,84 +22,146 @@ func Example() {
 	}
 	defer f.Close()
 
-	// var list event.List
-	// stream := trace.Stream(f)
-	//
-	// fmt.Println(`done`, stream)
+	var (
+		evt event.Event
+		d   = encoding.NewDecoder(f)
+	)
+	for d.More() {
+		evt.Reset()
+		if err := d.Decode(&evt); err != nil {
+			break
+		}
+		if evt.Type == event.EvGoSysCall {
+			fmt.Println(evt.Type) // print syscall events
+		}
+	}
+	if err := d.Err(); err != nil {
+		fmt.Println(`Err: `, err)
+		return
+	}
 
 	// Output:
-	//
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
+	// event.GoSysCall
 }
 
-func Example_visit() {
-	f, err := os.Open(`internal/tracefile/testdata/go1.8/log.trace`)
+func Example_runtimeDecoding() {
+	sleepFn := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		<-time.After(time.Millisecond * 100)
+	}
+
+	r, w := io.Pipe()
+	trace.Start(w)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go sleepFn(&wg)
+	go sleepFn(&wg)
+	go sleepFn(&wg)
+	wg.Wait()
+
+	go func() {
+		defer w.Close()
+		trace.Stop()
+	}()
+
+	var (
+		dec  = encoding.NewDecoder(r)
+		evt  event.Event
+		evts []*event.Event
+	)
+
+	v, err := dec.Version()
 	if err != nil {
 		fmt.Println(`Err:`, err)
 		return
 	}
-	defer f.Close()
 
-	var (
-		list event.List
-	)
-	if err := trace.Visit(f, &list); err != nil {
+	tr, err := event.NewTrace(v)
+	if err != nil {
 		fmt.Println(`Err:`, err)
+		return
 	}
 
-	fmt.Println(`done`, list)
-
-	// Output:
-	//
-}
-
-// func Example_correlator() {
-// 	f, err := os.Open(`internal/tracefile/testdata/go1.8/log.trace`)
-// 	if err != nil {
-// 		fmt.Println(`Err:`, err)
-// 		return
-// 	}
-// 	defer f.Close()
-//
-// 	c := trace.Correlate(f)
-// 	correlate.With
-//
-// 	fmt.Println(`done`)
-//
-// 	// Output:
-// 	//
-// }
-
-/*
-	var i, created int
-	evt := new(event.Event)
-	dec := encoding.NewDecoder(bytes.NewReader(data))
 	for dec.More() {
-		err := dec.Decode(evt)
-		if err != nil {
-			break // err will be in Err()
+		evt.Reset()
+		if err := dec.Decode(&evt); err != nil {
+			break
 		}
-		if evt.Type == event.EvGoCreate {
-			created++
+		if err := tr.Visit(&evt); err != nil {
+			fmt.Println(`Err:`, err)
 		}
-		if i += 1; i%40 == 0 {
-			fmt.Println(evt.Type) // printing a sampling of data
-		}
+		evts = append(evts, evt.Copy())
 	}
 	if err := dec.Err(); err != nil {
-		fmt.Println(`Err:`, err)
+		fmt.Println(`Err: `, err)
+		return
 	}
-	fmt.Printf("\nCreated %v goroutines\n", created)
+
+	// Lets make sure a goroutine is started for this func
+	findPC := uint64(reflect.ValueOf(sleepFn).Pointer())
+	findName := runtime.FuncForPC(uintptr(findPC)).Name()
+
+	for _, e := range evts {
+		if e.Type != event.EvGoCreate {
+			continue
+		}
+
+		// We want a stack for the new StackID
+		stack := tr.Stacks[e.Get(`NewStackID`)]
+		if len(stack) < 1 {
+			continue
+		}
+
+		name := runtime.FuncForPC(uintptr(stack[0].PC())).Name()
+		if findName == name {
+			stack, ok := tr.Stacks[e.Get(`StackID`)]
+			if !ok {
+				fmt.Println(`No stack exists for event:`, e)
+			}
+
+			// can't print stack in a unit test due to lack of determnism
+			// fmt.Println(stack)
+			fmt.Printf("\nFound EvGoCreate for `sleepFn` with new stack:\n====\n")
+			for _, frame := range stack {
+				fmt.Printf("  %v\n", frame.Func())
+			}
+		}
+	}
 
 	// Output:
-	// encoding.HeapAlloc
-	// encoding.HeapAlloc
-	// encoding.HeapAlloc
-	// encoding.GoCreate
-	// encoding.ProcStop
-	// encoding.String
-	// encoding.String
-	// encoding.Stack
+	// Found EvGoCreate for `sleepFn` with new stack:
+	// ====
+	//   github.com/cstockton/go-trace_test.Example_runtimeDecoding
+	//   testing.runExample
+	//   testing.runExamples
+	//   testing.(*M).Run
+	//   main.main
 	//
-	// Created 12 goroutines
+	// Found EvGoCreate for `sleepFn` with new stack:
+	// ====
+	//   github.com/cstockton/go-trace_test.Example_runtimeDecoding
+	//   testing.runExample
+	//   testing.runExamples
+	//   testing.(*M).Run
+	//   main.main
+	//
+	// Found EvGoCreate for `sleepFn` with new stack:
+	// ====
+	//   github.com/cstockton/go-trace_test.Example_runtimeDecoding
+	//   testing.runExample
+	//   testing.runExamples
+	//   testing.(*M).Run
+	//   main.main
+
 }
-*/
